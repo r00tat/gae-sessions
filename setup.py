93a2060b6c7f17dba139aeef80f8d4ff58ada8a2
Paul Woelfel <paul.woelfel@zirrus.eu>
2014-11-06 10:29:07 +0100

    added setup.py for pip installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='gaesessions',
+      version='1.0.7',
+      author='David Underhill',
+      author_email='',
+      license='Apache License Version 2.0',
+      description='gae-sessions is a sessions library for the Python runtime on Google App Engine for ALL session sizes. ' +
+      'It is extremely fast, lightweight (one file), and easy to use.',
+      py_modules=['gaesessions'],
+      install_requires=[
+      ])
